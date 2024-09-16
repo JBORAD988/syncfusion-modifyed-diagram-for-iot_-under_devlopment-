699c3baed3f31bf827a3b67b83fea0522e6b8d49
Mitul <130025869+Mitul2711@users.noreply.github.com>
2024-09-16 13:17:09 +0000

    GridLineHide_Custome_Model

---
+++ src/app/Model/Custom-snapSetting-model.d.ts
@@ -0,0 +1,5 @@
+import {SnapSettingsModel } from "@syncfusion/ej2-diagrams/src/diagram/diagram/grid-lines-model";
+
+export interface CustomSnapSettingModel extends SnapSettingsModel {
+    hideDiagramGrid?: boolean;
+}
